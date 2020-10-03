016e34f7be08792a043743b114fb8b3866710dcc
Kahzerx <kahzer123@gmail.com>
2020-10-03 21:47:15 +0200

    acceso a datos

---
+++ AD/fileTesting/src/com/fileTest/Main.java
@@ -0,0 +1,37 @@
+package com.fileTest;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+
+public class Main {
+
+    public static String dirName = "directorio";
+
+    public static String fileName = String.format("%s/test.txt", dirName);
+
+    public static void main(String[] args) throws IOException {
+        boolean tryCreateDir = new File(dirName).mkdirs();
+        if (tryCreateDir) System.out.printf("Dir %s created.%n", dirName);
+        File file = new File(fileName);
+        boolean tryCreateFile = file.createNewFile();
+        if (tryCreateFile) System.out.printf("File %s created.%n", fileName);
+        if (file.exists()){
+            System.out.printf("Nombre: %s%n", file.getName());
+            System.out.printf("Directorio padre: %s%n", file.getParent());
+            System.out.printf("Ruta relativa: %s%n", file.getPath());
+            System.out.printf("Ruta absoluta: %s%n", file.getAbsolutePath());
+        }
+
+        // Con FileChooser
+
+        JFileChooser chooser = new JFileChooser();
+        int returnValue = chooser.showOpenDialog(null);
+        if(returnValue == JFileChooser.APPROVE_OPTION) {
+            System.out.printf("%nNombre: %s%n", chooser.getSelectedFile().getName());
+            System.out.printf("Directorio padre: %s%n", chooser.getCurrentDirectory().getName());
+            System.out.printf("Ruta relativa: %s%n", chooser.getSelectedFile().getPath());
+            System.out.printf("Ruta absoluta: %s%n", chooser.getSelectedFile().getAbsolutePath());
+        }
+    }
+}
