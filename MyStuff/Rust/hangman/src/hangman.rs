be6e396ba92355b2503707733495021b00fbed26
Kahzerx <kahzer123@gmail.com>
2020-11-26 00:34:47 +0100

    refactor

--- MyStuff/Rust/hangman/src/hangman.rs
+++ MyStuff/Rust/hangman/src/hangman.rs
@@ -1,5 +1,6 @@
 use std::process::Command;
 use crate::utils;
+use crate::utils::{Word, hyphen_writer, log, build_word};
 
 pub fn run() {
     let mut word = utils::Word::new();
@@ -8,10 +9,67 @@ pub fn run() {
         clear_console();
         utils::initialize(&mut word, vec!["CETYS", "TEST", "HOLA", "ADIOS", "BABYYODA"]);
 
-        utils::play(&mut word);
+        play(&mut word);
 
         if !utils::validate("Quieres jugar de nuevo?", vec!["S", "N"]) { break; }
         utils::reset(&mut word);
+    }
+}
+
+fn play(w: &mut Word) {
+    let mut s = String::new();
+    let mut has_finished = false;
+    let array: Vec<char> = w.word.chars().collect();
+
+    while !has_finished {
+        hyphen_writer(w.already_done.to_owned());  // Escribe las "_" de la palabra reemplazando las ya completadas.
+        print!("\nIntroduce una letra: ");
+        utils::read(&mut s);
+        s = s.to_uppercase().trim().parse().unwrap();  // Leo el input y lo parseo para quitar tabuladores e intros innecesarios.
+
+        if s.chars().count() == 1 {  // Compruebo que solo se ha introducido un char.
+
+            if array.contains(&s.chars().next().unwrap()) {  // Compruebo si el char existe en la palabra.
+
+                if !w.already_done.contains(&s) {  // Compruebo si ya he escrito la letra introducida.
+                    w.already_done = build_word(w.word.chars(), s.chars().next().unwrap(), w.already_done.to_owned());  // Actualizo mi array principal.
+
+                    if !w.already_done.contains(&"_".to_string()) {  // Compruebo si he completado todas las letras.
+                        log(vec!["GG"]);
+                        has_finished = true;
+                    }
+
+                    else {
+                        log(vec!["Intentos restantes", &w.tries.to_string()]);
+                    }
+                }
+
+                else {
+                    log(vec!["Ya has escrito esa letra!"]);
+                    log(vec!["Intentos restantes", &w.tries.to_string()]);
+                }
+            }
+
+            else {
+                log(vec!["Letra incorrecta"]);
+                w.tries -= 1;
+
+                if w.tries > 0 {  // Compruebo que no he agotado mi máximo de intentos.
+                    log(vec!["Intentos restantes", &w.tries.to_string()]);
+                }
+
+                else {
+                    log(vec!["RIP :("]);
+                    has_finished = true;
+                }
+            }
+        }
+
+        else {
+            log(vec!["Error, introduce una sola letra."]);
+            log(vec!["Intentos restantes", &w.tries.to_string()]);
+        }
+        s = "".parse().unwrap();  // Reseteo la variable, si no lo hago al proximo read() se hace un append.
     }
 }
 
--- MyStuff/Rust/hangman/src/utils.rs
+++ MyStuff/Rust/hangman/src/utils.rs
@@ -3,10 +3,10 @@ use std::io::{stdin, stdout, Write};
 use std::str::Chars;
 
 pub struct Word {
-    word: String,
-    already_done: Vec<String>,
+    pub(crate) word: String,
+    pub(crate) already_done: Vec<String>,
     word_list: Vec<String>,
-    tries: i8
+    pub(crate) tries: i8
 }
 
 impl Word {
@@ -40,64 +40,7 @@ pub fn reset(w: &mut Word) {
     w.tries = 6;
 }
 
-pub fn play(w: &mut Word) {
-    let mut s = String::new();
-    let mut has_finished = false;
-    let array: Vec<char> = w.word.chars().collect();
-
-    while !has_finished {
-        hyphen_writer(w.already_done.to_owned());  // Escribe las "_" de la palabra reemplazando las ya completadas.
-        print!("\nIntroduce una letra: ");
-        read(&mut s);
-        s = s.to_uppercase().trim().parse().unwrap();  // Leo el input y lo parseo para quitar tabuladores e intros innecesarios.
-
-        if s.chars().count() == 1 {  // Compruebo que solo se ha introducido un char.
-
-            if array.contains(&s.chars().next().unwrap()) {  // Compruebo si el char existe en la palabra.
-
-                if !w.already_done.contains(&s) {  // Compruebo si ya he escrito la letra introducida.
-                    w.already_done = build_word(w.word.chars(), s.chars().next().unwrap(), w.already_done.to_owned());  // Actualizo mi array principal.
-
-                    if !w.already_done.contains(&"_".to_string()) {  // Compruebo si he completado todas las letras.
-                        log(vec!["GG"]);
-                        has_finished = true;
-                    }
-
-                    else {
-                        log(vec!["Intentos restantes", &w.tries.to_string()]);
-                    }
-                }
-
-                else {
-                    log(vec!["Ya has escrito esa letra!"]);
-                    log(vec!["Intentos restantes", &w.tries.to_string()]);
-                }
-            }
-
-            else {
-                log(vec!["Letra incorrecta"]);
-                w.tries -= 1;
-
-                if w.tries > 0 {  // Compruebo que no he agotado mi máximo de intentos.
-                    log(vec!["Intentos restantes", &w.tries.to_string()]);
-                }
-
-                else {
-                    log(vec!["RIP :("]);
-                    has_finished = true;
-                }
-            }
-        }
-
-        else {
-            log(vec!["Error, introduce una sola letra."]);
-            log(vec!["Intentos restantes", &w.tries.to_string()]);
-        }
-        s = "".parse().unwrap();  // Reseteo la variable, si no lo hago al proximo read() se hace un append.
-    }
-}
-
-fn build_word(ltr: Chars, letter: char, mut actual_array: Vec<String>) -> Vec<String> {
+pub fn build_word(ltr: Chars, letter: char, mut actual_array: Vec<String>) -> Vec<String> {
     for (i, c) in ltr.enumerate() {
 
         if c == letter {
@@ -107,7 +50,7 @@ fn build_word(ltr: Chars, letter: char, mut actual_array: Vec<String>) -> Vec<St
     actual_array
 }
 
-fn hyphen_writer(w: Vec<String>) {
+pub fn hyphen_writer(w: Vec<String>) {
     println!("{}", w.join(" "));
 }
 
@@ -137,7 +80,7 @@ pub fn validate(s1: &str, opt: Vec<&str>) -> bool {
     s == opt[0].to_uppercase()
 }
 
-fn create_word_list(list: Vec<&str>) -> Vec<String> {
+pub fn create_word_list(list: Vec<&str>) -> Vec<String> {
     let mut final_list = Vec::new();
 
     for w in list {
@@ -147,11 +90,11 @@ fn create_word_list(list: Vec<&str>) -> Vec<String> {
     final_list
 }
 
-fn log(what: Vec<&str>) {
+pub fn log(what: Vec<&str>) {
     println!("{}", what.join(" "));
 }
 
-fn read(input: &mut String) {  // Read from console.
+pub(crate) fn read(input: &mut String) {  // Read from console.
     stdout().flush().expect("Failed to flush");
     stdin().read_line(input).expect("Failed to read");
 }
