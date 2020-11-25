d5e13e8dc32465f146f01e183b953dabc63d91c0
Kahzerx <kahzer123@gmail.com>
2020-11-26 00:25:10 +0100

    hangman v1.0

--- MyStuff/Rust/hangman/src/hangman.rs
+++ MyStuff/Rust/hangman/src/hangman.rs
@@ -1,15 +1,21 @@
+use std::process::Command;
 use crate::utils;
 
 pub fn run() {
     let mut word = utils::Word::new();
-    utils::initialize(&mut word, vec!["CETYS", "TEST", "HOLA", "ADIOS", "BABYYODA"]);
 
-    utils::play(&mut word);
+    loop {
+        clear_console();
+        utils::initialize(&mut word, vec!["CETYS", "TEST", "HOLA", "ADIOS", "BABYYODA"]);
 
+        utils::play(&mut word);
 
+        if !utils::validate("Quieres jugar de nuevo?", vec!["S", "N"]) { break; }
+        utils::reset(&mut word);
+    }
+}
 
-
-
-    utils::validate("Quieres jugar de nuevo?", vec!["S", "N"]);
-    utils::reset(&mut word);
+fn clear_console() {
+    let output = Command::new("cls").output().unwrap_or_else(|_|Command::new("clear").output().unwrap());
+    println!("{}", String::from_utf8_lossy(&output.stdout));
 }
--- MyStuff/Rust/hangman/src/utils.rs
+++ MyStuff/Rust/hangman/src/utils.rs
@@ -1,5 +1,6 @@
-use rand::seq::SliceRandom;
+use rand::seq::SliceRandom;  // Elegir una palabra random.
 use std::io::{stdin, stdout, Write};
+use std::str::Chars;
 
 pub struct Word {
     word: String,
@@ -18,86 +19,136 @@ impl Word {
         }
     }
 
-    fn randomize(&mut self) {
+    fn randomize(&mut self) -> Vec<String> {
         self.word = self.word_list.choose(&mut rand::thread_rng()).unwrap().to_string();
         let mut result = vec![];
         for _chr in self.word.chars() {
             result.push("_".to_string());
         }
-        self.already_done = result;
+
+        result
     }
 }
 
 pub fn initialize(w: &mut Word, list: Vec<&str>) {
-    create_word_list(w, list);
-    get_random_word(w);
+    w.word_list = create_word_list(list);
+    w.already_done = w.randomize();
 }
 
 pub fn reset(w: &mut Word) {
-    get_random_word(w);
+    w.already_done = w.randomize();
     w.tries = 6;
 }
 
 pub fn play(w: &mut Word) {
     let mut s = String::new();
-    let has_finished = false;
-    let array = w.word.split("") as Vec<String>;
+    let mut has_finished = false;
+    let array: Vec<char> = w.word.chars().collect();
+
     while !has_finished {
-        hyphen_writer(w);
+        hyphen_writer(w.already_done.to_owned());  // Escribe las "_" de la palabra reemplazando las ya completadas.
         print!("\nIntroduce una letra: ");
         read(&mut s);
-        s = s.to_uppercase().trim().parse().unwrap();
-        if s.chars().count() == 1 {
-            if array.contains(&s) {
-                if !w.already_done.contains(&s) {
-                    w.tries -= 1;
+        s = s.to_uppercase().trim().parse().unwrap();  // Leo el input y lo parseo para quitar tabuladores e intros innecesarios.
+
+        if s.chars().count() == 1 {  // Compruebo que solo se ha introducido un char.
+
+            if array.contains(&s.chars().next().unwrap()) {  // Compruebo si el char existe en la palabra.
+
+                if !w.already_done.contains(&s) {  // Compruebo si ya he escrito la letra introducida.
+                    w.already_done = build_word(w.word.chars(), s.chars().next().unwrap(), w.already_done.to_owned());  // Actualizo mi array principal.
+
+                    if !w.already_done.contains(&"_".to_string()) {  // Compruebo si he completado todas las letras.
+                        log(vec!["GG"]);
+                        has_finished = true;
+                    }
+
+                    else {
+                        log(vec!["Intentos restantes", &w.tries.to_string()]);
+                    }
                 }
+
                 else {
-                    println!("Ya has escrito esa letra!")
+                    log(vec!["Ya has escrito esa letra!"]);
+                    log(vec!["Intentos restantes", &w.tries.to_string()]);
                 }
             }
+
             else {
-                println!("Letra incorrecta.");
+                log(vec!["Letra incorrecta"]);
+                w.tries -= 1;
+
+                if w.tries > 0 {  // Compruebo que no he agotado mi máximo de intentos.
+                    log(vec!["Intentos restantes", &w.tries.to_string()]);
+                }
+
+                else {
+                    log(vec!["RIP :("]);
+                    has_finished = true;
+                }
             }
         }
+
         else {
-            println!("Error");
+            log(vec!["Error, introduce una sola letra."]);
+            log(vec!["Intentos restantes", &w.tries.to_string()]);
         }
+        s = "".parse().unwrap();  // Reseteo la variable, si no lo hago al proximo read() se hace un append.
     }
 }
 
-fn hyphen_writer(w: &Word) {
-    println!("{}", w.already_done.join(" "));
+fn build_word(ltr: Chars, letter: char, mut actual_array: Vec<String>) -> Vec<String> {
+    for (i, c) in ltr.enumerate() {
+
+        if c == letter {
+            actual_array[i] = c.to_string();
+        }
+    }
+    actual_array
+}
+
+fn hyphen_writer(w: Vec<String>) {
+    println!("{}", w.join(" "));
 }
 
 pub fn validate(s1: &str, opt: Vec<&str>) -> bool {
     let mut s = String::new();
     let mut is_valid = false;
+
     while !is_valid {
         s = "".to_string();
         print!("\n{} ({}): ", s1, opt.join("/"));
         read(&mut s);
         s = s.to_uppercase().trim().parse().unwrap();
+
+        if s == "" {
+            return true;
+        }
+
         if s.chars().count() == 1 && opt.contains(&&*s) {
             is_valid = true;
         }
+
         else {
-            println!("Introduce una opción correcta.");
+            log(vec!["Introduce una opción correcta."]);
         }
     }
-    return s.to_ascii_uppercase().trim() == opt[0];
+
+    s == opt[0].to_uppercase()
 }
 
-fn create_word_list(mut w: &mut Word, list: Vec<&str>) {
+fn create_word_list(list: Vec<&str>) -> Vec<String> {
     let mut final_list = Vec::new();
+
     for w in list {
         final_list.push(w.to_string());
     }
-    w.word_list = final_list;
+
+    final_list
 }
 
-fn get_random_word(w: &mut Word) {
-    w.randomize();
+fn log(what: Vec<&str>) {
+    println!("{}", what.join(" "));
 }
 
 fn read(input: &mut String) {  // Read from console.
