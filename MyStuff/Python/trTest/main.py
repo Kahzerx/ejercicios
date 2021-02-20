82c58583b8c8e24980198824a8dc9db027f06104
Kahzerx <kahzerx@tutanota.com>
2021-02-20 15:16:24 +0100

    get from on

--- MyStuff/Python/trTest/main.py
+++ MyStuff/Python/trTest/main.py
@@ -1,3 +1,5 @@
+# https://jarroba.com/anonymous-scraping-by-tor-network/
+
 import requests
 from time import sleep
 from stem import Signal
@@ -28,6 +30,11 @@ def get_tor_connection():
         proxies = {'http': 'socks5://127.0.0.1:9050', 'https': 'socks5://127.0.0.1:9050'}
         result = s.get("https://ipinfo.io/", proxies=proxies).text
         print(result)
+        # socks5h protocol in order to enable remote DNS resolving in case the local DNS resolving process fails.
+        # See https://github.com/kennethreitz/requests/blob/e3f89bf23c53b98593e4248054661472aacac820/requests/packages/urllib3/contrib/socks.py#L158
+        proxies2 = {'http': 'socks5h://127.0.0.1:9050', 'https': 'socks5h://127.0.0.1:9050'}
+        result2 = s.get("https://3g2upl4pq6kufc4m.onion/", proxies=proxies2).text
+        print(result2)
 
 
 def renew_connection(time):
